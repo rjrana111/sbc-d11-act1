34aa149b6e377a34e9b52cb451b7bbfd7ea53c08
rjrana111 <roque.rana@aclcbutuan.edu.ph>
2024-07-11 14:09:20 +0800

    first commit

---
+++ try.py
@@ -0,0 +1,16 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+                print(arr)
+        if not swapped:
+            break
+    return arr
+
+unsorted = ["D", "S", "M", "R", "A", "E"]
+sorted_arr = bubble_sort(unsorted)
+print("Sorted array:", sorted_arr)
